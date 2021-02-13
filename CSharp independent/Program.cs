8e64e661e97b9e50a90c57513471bafefe0c902e
Mark Maurer <markmaurer1427@gmail.com>
2021-02-12 20:18:24 -0600

    init commit

---
+++ CSharp independent/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharp_independent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursion(10);
+        }
+
+        public static void Recursion(int x)
+        {
+            if (x == 0)
+            {
+                Console.WriteLine("A for loop is easier than this.");
+                Console.WriteLine("Recursion done.");
+            }
+            else
+            {
+                Console.WriteLine("A for loop is easier than this.");
+                x--;
+                Recursion(x);
+            }
+        }
+    }
+}

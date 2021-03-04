ecefbf6ca7e9576fbf0b760babfe41be91837998
Mark Maurer <markmaurer1427@gmail.com>
2021-03-03 19:24:52 -0600

    Method overload exercise

---
+++ CSharp independent/MethodOverload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_independent
+{
+    class MethodOverload
+    {
+        public static void TypeOut(int x)
+        {
+            Console.WriteLine("You input an INT parameter");
+        }
+
+        public static void TypeOut(double x)
+        {
+            Console.WriteLine("You input a DOUBLE parameter");
+        }
+
+        public static void TypeOut(string x)
+        {
+            Console.WriteLine("You input a STRING parameter");
+        }
+
+        public static void TypeOut(int[] x)
+        {
+            Console.WriteLine("You input an INT array");
+        }
+
+        public static void TypeOut(double[] x)
+        {
+            Console.WriteLine("You input a DOUBLE array");
+        }
+        public static void TypeOut(string[] x)
+        {
+            Console.WriteLine("You input a STRING array");
+        }
+    }
+}
--- CSharp independent/Program.cs
+++ CSharp independent/Program.cs
@@ -6,10 +6,25 @@ namespace CSharp_independent
     {
         static void Main(string[] args)
         {
+            int i1 = 1;
+            double d1 = 1.5;
+            string s1 = "string";
+            int[] i2 = { 0, 1, 2, 3 };
+            double[] d2 = { 1.5, 2.6, 3.7 };
+            string[] s2 = { "Thing 1", "Thing 2" };
+
             //Voting.VoteCheck();
             //NumGuess.Guess();
             //Plinko.drop();
+            MethodOverload.TypeOut(i1);
+            MethodOverload.TypeOut(i2);
+            MethodOverload.TypeOut(d1);
+            MethodOverload.TypeOut(d2);
+            MethodOverload.TypeOut(s1);
+            MethodOverload.TypeOut(s2);
         }
+
+
 
     }
 }
